fb030ca45f6e36898ab97cbacb5ef6f75c8bfc39
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-14 09:12:30 -0700

    went back and added the snapshot test

---
+++ src/components/Dog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Dog() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Yeller</dd>
+
+      <dt>Age</dt>
+      <dd>15</dd>
+
+      <dt>Weight</dt>
+      <dd>60 lbs</dd>
+    </dl>
+  );
+}
+
